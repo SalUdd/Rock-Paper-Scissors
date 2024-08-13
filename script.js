c6344d1683b10410add619cc4b8251f19cce17bb
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-08-13 14:50:27 +1000

    created function with nested ifs to determine the winner based on the users inputs and computers choice from array, added counters to increase the score of the computer and user on their wins and to do nothing on losses and draws other then a message

--- script.js
+++ script.js
@@ -18,6 +18,44 @@ function getComputerChoice(){
         
     }
     
+    function playRound(humanChoice, computerChoice) {
+        // your code here!
+        if (humanChoice === 'rock' && computerChoice === 'Paper'){
+             console.log("You lose! Paper beats Rock")
+             computerScore++;
+        } else if (humanChoice === 'rock' && computerChoice === 'Scissors') {
+            console.log("You Win! Rock beats Scissor")
+            humanScore++;
+        } else if (humanChoice === 'rock' && computerChoice === 'Rock'){
+            console.log("It's a DRAW! Rock vs Rock")
+        } 
+        
+        else if (humanChoice === 'scissors' && computerChoice === 'Paper'){
+                console.log("You Win! Scissors beats Paper")
+                humanScore++;
+        } else if (humanChoice ===  'scissors' && computerChoice === 'Scissors') {
+                console.log("It's a DRAW! Scissors vs Scissors")
+        } else if (humanChoice === 'scissors' && computerChoice === 'Rock'){
+                console.log("You lose! Rock beats Scissor")
+                computerScore++;
+        } 
+        
+        else if (humanChoice === 'paper' && computerChoice === 'Paper'){
+            console.log("It's a DRAW! Paper vs Paper")
+        } else if (humanChoice ===  'paper' && computerChoice === 'Scissors') {
+            console.log("You lose! Scissors beats Paper")
+            computerScore++;
+        } else if (humanChoice === 'paper' && computerChoice === 'Rock'){
+            console.log("You Win! Paper beats Rock")
+            humanScore++;
+        } 
+        
+        
+        else 
+        {
+            console.log("Please choose an appropriate option")
+        }
+      }
+      
+    
  
-    
-    
